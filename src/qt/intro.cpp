5648845bb5fc43c3931bb167fa5346273477972d
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2024-02-05 11:35:43 -0800

    Fix date

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -128,7 +128,7 @@ Intro::Intro(QWidget *parent) :
     ui->lblExplanation1->setText(ui->lblExplanation1->text()
         .arg(tr(PACKAGE_NAME))
         .arg(BLOCK_CHAIN_SIZE)
-        .arg(2018)
+        .arg(2023)
         .arg(tr("Raianetwork"))
     );
     ui->lblExplanation2->setText(ui->lblExplanation2->text().arg(tr(PACKAGE_NAME)));
